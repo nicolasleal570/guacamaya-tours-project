1ebe54b40a76fa966bf7d66d7e2599776e2ed2e4
nicolasleal570 <lealnicolas96@gmail.com>
2019-10-11 09:43:55 -0400

    Creating the home page with static data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientLayoutComponent } from './components/client-layout/client-layout.component';
+import { HomePageComponent } from './components/client-layout/home-page/home-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ClientLayoutComponent, children: [
+    { path: '', component: HomePageComponent }
+  ] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientLayoutComponent } from './components/client-layout/client-layout.component';
+import { NavbarComponent } from './components/client-layout/partials/navbar/navbar.component';
+import { HomePageComponent } from './components/client-layout/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientLayoutComponent,
+    NavbarComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
