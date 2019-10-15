9b61ad59a3f859a1b127d4933f1bf18d2a5b7d21
nicolasleal570 <lealnicolas96@gmail.com>
2019-10-15 18:53:23 -0400

    Fixing bugs with imports of components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ClientLayoutComponent } from './components/client-layout/client-layout.component';
-import { HomePageComponent } from './components/client-layout/home-page/home-page.component';
 
 
 const routes: Routes = [];
