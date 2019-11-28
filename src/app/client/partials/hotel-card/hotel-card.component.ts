fc180da23da206b5cd3efb18bc131fb4ae5dbc96
Elena Martellacci <elena.7moya@gmail.com>
2019-11-28 10:50:40 -0400

    fecha de itinerario si nterminar

--- src/app/client/partials/hotel-card/hotel-card.component.ts
+++ src/app/client/partials/hotel-card/hotel-card.component.ts
@@ -11,10 +11,25 @@ export class HotelCardComponent implements OnInit {
   @Input() hotel: Hotel;
   @Input() darkColor: boolean = false;
   @Input() footer: boolean = false;
+  estrellas: number[];
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
+    this.hacerEstrellas();
+  }
+
+  hacerEstrellas() {
+    for ( var i = 0; i++; i<5) {
+    if (i < this.hotel.stars) {
+      this.estrellas[i] = 1;
+    }else{
+      this.estrellas[i] = 0;
+    }
+    console.log(this.estrellas[i]);
+  }
   }
 
 }
